35be3e740d9b7f0fbbeb5a823aa44b6bea34f757
Can Kolay <cankolay3499@gmail.com>
2023-03-08 23:44:23 +0300

    feat: allow chaning theme without editing the config

---
+++ lua/oxygen/base46/themes/init.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.themes = {
+  'cattpuccin',
+  'cattpuccin_latte',
+  'gruvbox',
+  'gruvbox_light',
+  'monokai_dimmed',
+  'nord',
+  'onedark',
+  'onelight',
+  'rosepine',
+  'tokyonight',
+  'tundra',
+}
+
+return M
