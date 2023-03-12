c27500687ba8fe7b67304647dac733bf802651de
budchirp <budchirp@gmail.com>
2023-03-12 23:42:16 +0300

    feat: add new highlights of extensions

--- lua/oxygen/base46/highlights/extensions.lua
+++ lua/oxygen/base46/highlights/extensions.lua
@@ -1,8 +1,12 @@
 local colors = base46.get_theme_tb('base_30')
 
 return {
-      ['@oxygen.updater.name'] = { bg = colors.grren, fg = colors.white },
-      ['@oxygen.updater.title'] = { fg = colors.white },
-      ['@oxygen.updater.description'] = { fg = colors.blue },
-      ['@oxygen.updater.description.error'] = { fg = colors.red },
+  ['OxygenUpdaterName'] = { bg = colors.cyan, fg = colors.black },
+  ['OxygenUpdaterTitle'] = { fg = colors.white, bg = colors.black2 },
+  ['OxygenUpdaterTitleError'] = { fg = colors.baby_pink, bg = colors.black2 },
+  ['OxygenUpdaterTitleIcon'] = { bg = colors.green, fg = colors.black },
+  ['OxygenUpdaterTitleIconError'] = { bg = colors.baby_pink, fg = colors.black },
+  ['OxygenUpdaterDescription'] = { bg = colors.black2, fg = colors.white },
+  ['OxygenUpdaterDescriptionHash'] = { bg = colors.black2, fg = colors.green },
+  ['OxygenUpdaterDescriptionError'] = { bg = colors.baby_pink, fg = colors.black },
 }
